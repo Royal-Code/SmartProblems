504d93e2758d8c2335c930e0c30c709e2994ce25
eglauko <eglauko@hotmail.com>
2024-04-25 09:59:16 -0300

    Added Validations

---
+++ src/RoyalCode.SmartProblems.FluentValidation/ExtensionData.cs
@@ -0,0 +1,45 @@
+﻿using RoyalCode.SmartProblems;
+
+namespace FluentValidation;
+
+/// <summary>
+/// <para>
+///     A readonly struct to add extra data to the validation result.
+/// </para>
+/// <para>
+///     The data is stored in a dictionary and will be available in the <see cref="Problem.Extensions"/> property.
+/// </para>
+/// </summary>
+public readonly struct ExtensionData
+{
+    private readonly Dictionary<string, object> data;
+
+    /// <summary>
+    /// Create a new instance of <see cref="ExtensionData"/>.
+    /// </summary>
+    /// <param name="data">The dictionary to add extra data.</param>
+    public ExtensionData(Dictionary<string, object> data)
+    {
+        this.data = data;
+    }
+
+    /// <summary>
+    /// Add extra data to the validation result.
+    /// </summary>
+    /// <param name="key">The key of the extra data.</param>
+    /// <param name="value">The value of the extra data.</param>
+    /// <returns>
+    ///     The same instance of <see cref="ExtensionData"/> for chaining.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     Case the <paramref name="key"/> is null.
+    /// </exception>
+    /// <exception cref="AggregateException">
+    ///     Case the <paramref name="key"/> already exists in the dictionary.
+    /// </exception>
+    public ExtensionData Add(string key, object value)
+    {
+        data.Add(key, value);
+        return this;
+    }
+}
---
+++ src/RoyalCode.SmartProblems.FluentValidation/ValidationsExtensions.cs
@@ -0,0 +1,127 @@
+﻿using FluentValidation.Results;
+using RoyalCode.SmartProblems;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FluentValidation;
+
+/// <summary>
+/// Extensions methods to converver FluentValidation errors to <see cref="Result"/> and other operations.
+/// </summary>
+public static class ValidationsExtensions
+{
+    /// <summary>
+    /// The <see cref="ProblemCategory"/> used to convert FluentValidation errors to <see cref="Problem"/>.
+    /// </summary>
+    public static ProblemCategory ProblemCategory { get; set; } = ProblemCategory.InvalidParameter;
+
+    /// <summary>
+    /// Convert a list fo <see cref="ValidationFailure"/> from FluentValidations to <see cref="Problems"/>.
+    /// </summary>
+    /// <param name="errors">A list of <see cref="ValidationFailure"/>.</param>
+    /// <returns>The <see cref="Problems"/> converted.</returns>
+    public static Problems ToProblems(this IList<ValidationFailure> errors)
+    {
+        var problems = new Problems();
+
+        for (var i = 0; i < errors.Count; i++)
+        {
+            var error = errors[i];
+            switch (error.Severity)
+            {
+                case Severity.Warning:
+                case Severity.Info:
+                    break;
+                default:
+
+                    var problem = new Problem()
+                    {
+                        Category = ProblemCategory,
+                        Detail = error.ErrorMessage,
+                        Property = error.PropertyName,
+                    };
+
+                    if (error.CustomState is Dictionary<string, object> extensions)
+                    {
+                        problem.Extensions = extensions;
+                    }
+
+                    problem.With("error_code", error.ErrorCode);
+                    problems.Add(problem);
+
+                    break;
+            }
+        }
+
+        return problems;
+    }
+
+    /// <summary>
+    /// Convert a <see cref="ValidationResult"/> from FluentValidations to <see cref="Result"/>.
+    /// </summary>
+    /// <param name="result">The <see cref="ValidationResult"/> to convert.</param>
+    /// <returns>The <see cref="Result"/> converted.</returns>
+    public static Result ToResult(this ValidationResult result)
+    {
+        return result.IsValid
+            ? Result.Ok()
+            : result.Errors.ToProblems();
+    }
+
+    /// <summary>
+    /// Check if a <see cref="ValidationResult"/> has problems.
+    /// </summary>
+    /// <param name="result">The <see cref="ValidationResult"/> from FluentValidation to check.</param>
+    /// <param name="problems">The <see cref="Problems"/> if the result has problems.</param>
+    /// <returns>
+    ///     True if the <see cref="ValidationResult"/> has is invalid and has problems, otherwise false.
+    /// </returns>
+    public static bool HasProblems(this ValidationResult result, [NotNullWhen(true)] out Problems? problems)
+    {
+        return result.ToResult().HasProblems(out problems);
+    }
+
+    /// <summary>
+    /// Execute the validation of a model and check if it has problems.
+    /// </summary>
+    /// <typeparam name="T">The type of the model to validate.</typeparam>
+    /// <param name="validator">The model validator.</param>
+    /// <param name="model">The model instance to validate.</param>
+    /// <param name="problems">The <see cref="Problems"/> if the model is invalid.</param>
+    /// <returns>
+    ///     True if the model is invalid and has problems, otherwise false.
+    /// </returns>
+    public static bool HasProblems<T>(this AbstractValidator<T> validator, T model, [NotNullWhen(true)] out Problems? problems)
+    {
+        return validator.Validate(model).ToResult().HasProblems(out problems);
+    }
+
+    /// <summary>
+    /// <para>
+    ///     Adds extra data to the validation result that will be used to generate problems.
+    /// </para>
+    /// <para>
+    ///     This extra data will be available in the <see cref="Problem.Extensions"/> property.
+    /// </para>
+    /// </summary>
+    /// <typeparam name="T">The type of the model to validate.</typeparam>
+    /// <typeparam name="TProperty">The type of the property to validate.</typeparam>
+    /// <param name="rule">The rule builder options.</param>
+    /// <param name="action">The action to add extra data to the validation result.</param>
+    /// <returns>
+    ///     The same instance of <paramref name="rule"/> for chaining.
+    /// </returns>
+    public static IRuleBuilderOptions<T, TProperty> WithExtension<T, TProperty>(
+        this IRuleBuilderOptions<T, TProperty> rule,
+        Action<ExtensionData> action)
+    {
+        var component = DefaultValidatorOptions.Configurable(rule).Current;
+        component.CustomStateProvider = (ctx, prop) =>
+        {
+            var extensions = new Dictionary<string, object>();
+            action(new ExtensionData(extensions));
+            return extensions;
+        };
+
+        return rule;
+    }
+}
