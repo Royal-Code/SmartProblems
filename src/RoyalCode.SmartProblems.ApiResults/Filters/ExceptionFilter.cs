3b935878677f939842e41a4ef3dbc27ee901b988
Glauco Knihs <glauco@havan.com.br>
2024-08-01 18:25:40 -0300

    filters

---
+++ src/RoyalCode.SmartProblems.ApiResults/Filters/ExceptionFilter.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using RoyalCode.SmartProblems.HttpResults;
+
+namespace RoyalCode.SmartProblems.Filters;
+
+internal sealed class ExceptionFilter
+{
+    private readonly string displayName;
+    private readonly LogLevel logLevel;
+    private readonly ILogger logger;
+    private readonly EndpointFilterDelegate next;
+
+    public ExceptionFilter(string displayName, LogLevel logLevel, ILogger logger, EndpointFilterDelegate next)
+    {
+        this.displayName = displayName;
+        this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        this.next = next ?? throw new ArgumentNullException(nameof(next));
+        this.logLevel = logLevel;
+    }
+
+    public async ValueTask<object?> Handle(EndpointFilterInvocationContext context)
+    {
+        try
+        {
+            var result = await next(context);
+
+            if (logLevel != LogLevel.None
+                && result is INestedHttpResult nested
+                && nested.Result is MatchErrorResult matchError)
+            {
+                logger.Log(
+                    logLevel,
+                    "The endpoint result ({Endpoint}) is failure, problem(s): {Problems}",
+                    displayName,
+                    matchError);
+            }
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(
+                ex,
+                "An exception occurred during the execution of an API endpoint ({Endpoint})",
+                displayName);
+
+            MatchErrorResult result = Problems.InternalError(ex);
+            return result;
+        }
+    }
+}
---
+++ src/RoyalCode.SmartProblems.ApiResults/Filters/ExceptionFilterExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RoyalCode.SmartProblems.Filters;
+using RoyalCode.SmartProblems.HttpResults;
+
+namespace Microsoft.AspNetCore.Builder;
+
+/// <summary>
+/// Extensions methods for <see cref="IEndpointConventionBuilder"/>.
+/// </summary>
+public static class ExceptionFilterExtensions
+{
+    /// <summary>
+    /// Adds a filter to the MinimalApi pipeline to treat exceptions as Problem Details.
+    /// </summary>
+    /// <typeparam name="TBuilder"></typeparam>
+    /// <param name="ecb">The <see cref="IEndpointConventionBuilder"/>.</param>
+    /// <param name="logLevel">The log level, used to log <see cref="MatchErrorResult"/> when returned by the API.</param>
+    /// <param name="loggerType">A type, for create the <see cref="ILogger"/> from the <see cref="ILoggerFactory"/>.</param>
+    /// <returns>The same instance of <paramref name="ecb"/>.</returns>
+    public static TBuilder WithExceptionFilter<TBuilder>(
+        this TBuilder ecb,
+        LogLevel logLevel = LogLevel.None,
+        Type? loggerType = null) where TBuilder : IEndpointConventionBuilder
+    {
+        ecb.Add(builder =>
+        {
+            var displayName = builder.DisplayName;
+            builder.FilterFactories.Add(CreateFactory(displayName, logLevel, loggerType));
+        });
+        return ecb;
+    }
+
+    private static Func<EndpointFilterFactoryContext, EndpointFilterDelegate, EndpointFilterDelegate> CreateFactory(
+        string? displayName,
+        LogLevel logLevel,
+        Type? loggerType)
+    {
+        return (routeHandlerContext, next) =>
+        {
+            var loggerFactory = routeHandlerContext.ApplicationServices.GetRequiredService<ILoggerFactory>();
+            var logger = loggerType is not null
+                ? loggerFactory.CreateLogger(loggerType)
+                : loggerFactory.CreateLogger<ExceptionFilter>();
+
+            var filter = new ExceptionFilter(displayName ?? "Unknown Endpoint", logLevel, logger, next);
+            return filter.Handle;
+        };
+    }
+}
